66a24357d076000ed962542f2ffb78ee13c7ca38
Ahtisham-damda <ahtishamdamda0747@gmail.com>
2025-10-04 19:41:11 -0700

    exam

---
+++ Js-Exam/Tables.js
@@ -0,0 +1,16 @@
+// function table() {
+//     let n = 5
+//   for (let i = 1; i <= 10; i++) {
+//     let result = n * i;
+//     console.log(`${n} X ${i} = ${result} `);
+//   }
+//   console.log();
+// }
+// table(5);
+
+let n = 5;
+for (let i = 1; i <= 10; i++) {
+    let result = n * i;
+    console.log(`${n} X ${i} = ${result} `);
+  }
+  console.log();
---
+++ Js-Exam/area.js
@@ -0,0 +1,10 @@
+function area(l,b){
+    console.log(`Length of rectangle is ${l}`);
+        console.log(`breadth of rectangle is ${b}`);
+
+        let area = l * b;
+
+    console.log(`Area of Rectangle is ${area}`);
+    
+}
+area(10,6);
